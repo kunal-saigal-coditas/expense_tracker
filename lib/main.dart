2e1dc59ec4986068ff601b9f9b5ee9d08854b17a
kunal.saigal-coditas <kunal.saigal@coditas.com>
2023-03-21 14:36:00 +0530

    expense Tracker

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tracker Your Expenses",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker"),
+      ),
+      body: Center(
+        child: Text("Widget Playground!"),
+      ),
+    );
+  }
+}
