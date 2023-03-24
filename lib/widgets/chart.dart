baf65bfc3804cb2af593ed9a3a00f3fddb1e94fb
kunal.saigal-coditas <kunal.saigal@coditas.com>
2023-03-24 17:29:37 +0530

    m

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,28 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key key}) : super(key: key);
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      return {"day": "T", "amount": 2000};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
   }
 }
